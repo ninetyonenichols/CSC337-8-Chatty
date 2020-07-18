57702ae23bad85de6f8c9726e1d04cf00c2833bb
Justin Nichols <ninetyonenichols@gmail.com>
2020-07-18 19:49:39 +0000

    Added more server code, including some Mongoose.

--- chatty/public_html/communicator.js
+++ chatty/public_html/communicator.js
@@ -5,4 +5,12 @@
  */
 
 function send() {
+  $.ajax({
+    url: '/chats/post',
+    data: { alias: $('#input_alias').val(),
+      msg: $('#input_msg').val() },
+    method: 'POST',
+    success: () => console.log({ alias: $('#input_alias').val(),
+      msg: $('#input_msg').val() })
+  });
 }
--- chatty/server.js
+++ chatty/server.js
@@ -5,11 +5,30 @@
  */
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const parser = require('body-parser');
+
 const host = '64.227.49.233';
 const port = 80;
 
+const app = express();
+const db = mongoose.connection;
+const mongoDBURL = `mongodb://${host}/chats`;
+
+mongoose.connect(mongoDBURL, { useNewUrlParser: true });
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+var Schema = mongoose.Schema;
+var ChatMessageSchema = new Schema({
+  time: Number,
+  alias: String,
+  message: String
+});
+var ChatMessage = mongoose.model('ChatMessage', ChatMessageSchema );
+
 app
   .use(express.static('public_html'))
+  .get('/chats', (req, res) => res.redirect('/index.html'))
+  .post('/chats/post', (req, res) => res.redirect('/'))
   .all('*', (req, res) => res.redirect('/'))
   .listen(port, () => console.log('App listening'))
